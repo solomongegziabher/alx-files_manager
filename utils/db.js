fb0a9744fb402b3b51d62bbab3abc94c9db29230
Solomon <113886983+solomongegziabher@users.noreply.github.com>
2023-10-04 14:19:32 +0300

    Files manager

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,6 @@ import { MongoClient } from 'mongodb';
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
-
 const url = `mongodb://${HOST}:${PORT}`;
 
 class DBClient {
